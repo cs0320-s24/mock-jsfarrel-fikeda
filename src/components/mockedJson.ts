77cb31fe463cb74310685ad7971f6c0700019e90
fukaikeda <fuka_ikeda@brown.edu>
2024-02-25 19:19:00 -0500

    feat: added mocking files

--- src/components/mockedJson.ts
+++ src/components/mockedJson.ts
@@ -1,17 +1,37 @@
 const csvData = [
     ['Street', 'Price', 'Bedrooms', 'Bathrooms', 'Sqft', 'Zip', 'City', 'State', 'Type'],
     ['Brown St', '100000', '3', '2', '1500', '12345', 'Providence', 'RI', 'House'],
-    ['Green St', '200000', '4', '3', '2000', '12235', 'East Providence', 'RI', 'House'],
-    ['Blue St', '300000', '5', '4', '2500', '12023', 'Warwick', 'RI', 'Apartment'],
-    ['Red St', '400000', '6', '5', '3000', '12345', 'Providence', 'RI', 'House'],
-    ['Yellow St', '500000', '7', '6', '3500', '12345', 'Providence', 'RI', 'Townhouse'],
+    ['Green St', '200000', '3', '3', '2000', '12235', 'East Providence', 'RI', 'House'],
+    ['Blue St', '300000', '2', '4', '2500', '12023', 'Warwick', 'RI', 'Apartment'],
+    ['Red St', '400000', '3', '5', '3000', '12345', 'Providence', 'RI', 'House'],
+    ['Yellow St', '500000', '3', '6', '3500', '12345', 'Providence', 'RI', 'Townhouse'],
     ['Orange St', '6000', '2', '4', '400', '12345', 'Newport', 'RI', 'Apartment'],
     ['Purple St', '250000', '3', '2', '1800', '12346', 'Cranston', 'RI', 'House'],
     ['Silver St', '180000', '2', '1', '1200', '12347', 'Pawtucket', 'RI', 'Apartment'],
-    ['Gold St', '350000', '4', '3', '2200', '12348', 'Johnston', 'RI', 'Townhouse'],
-    ['White St', '450000', '5', '4', '2800', '12349', 'Barrington', 'RI', 'House'],
+    ['Gold St', '350000', '3', '3', '2200', '12348', 'Johnston', 'RI', 'Townhouse'],
+    ['White St', '450000', '2', '4', '2800', '12349', 'Barrington', 'RI', 'House'],
     ['Black St', '200000', '3', '2', '1600', '12350', 'Bristol', 'RI', 'House'],
-    ['Gray St', '320000', '4', '3', '2000', '12351', 'Woonsocket', 'RI', 'Townhouse']
+    ['Gray St', '320000', '2', '3', '2000', '12351', 'Woonsocket', 'RI', 'Townhouse']
+];
+
+const searchResultsBrown = [
+    ['Brown St', '100000', '3', '2', '1500', '12345', 'Providence', 'RI', 'House']
+];
+
+const searchResultsGold = [
+    ['Gold St', '350000', '3', '3', '2200', '12348', 'Johnston', 'RI', 'Townhouse']
+];
+
+const searchResultsInvalid = [
+    []
+];
+
+const searchResults2Bed = [
+    ['Blue St', '300000', '2', '4', '2500', '12023', 'Warwick', 'RI', 'Apartment'],
+    ['Orange St', '6000', '2', '4', '400', '12345', 'Newport', 'RI', 'Apartment'],
+    ['Silver St', '180000', '2', '1', '1200', '12347', 'Pawtucket', 'RI', 'Apartment'],
+    ['White St', '450000', '2', '4', '2800', '12349', 'Barrington', 'RI', 'House'],
+    ['Gray St', '320000', '2', '3', '2000', '12351', 'Woonsocket', 'RI', 'Townhouse']
 ];
 
 const csvDataAlt = [
@@ -23,7 +43,6 @@ const csvDataAlt = [
     ['Turtle St', '28000', 'Providence', 'RI', 'Townhouse']
 ];
 
-const searchResultsGold = [
-    ['Gold St', '350000', '4', '3', '2200', '12348', 'Johnston', 'RI', 'Townhouse']
+const searchResultsDog = [
+    ['Dog Ave', '100000', 'Providence', 'RI', 'House']
 ];
-
