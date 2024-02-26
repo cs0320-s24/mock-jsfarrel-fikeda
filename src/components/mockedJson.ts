34d7a22ea2c8c3ebdd9237e0c88c44f22da45999
fukaikeda <fuka_ikeda@brown.edu>
2024-02-25 19:03:31 -0500

    feat: created mocked json file

---
+++ src/components/mockedJson.ts
@@ -0,0 +1,20 @@
+const csvData = [
+    ['Street', 'Price', 'Bedrooms', 'Bathrooms', 'Sqft', 'Zip', 'City', 'State', 'Type'],
+    ['Brown St', '100000', '3', '2', '1500', '12345', 'Providence', 'RI', 'House'],
+    ['Green St', '200000', '4', '3', '2000', '12235', 'East Providence', 'RI', 'House'],
+    ['Blue St', '300000', '5', '4', '2500', '12023', 'Warwick', 'RI', 'Apartment'],
+    ['Red St', '400000', '6', '5', '3000', '12345', 'Providence', 'RI', 'House'],
+    ['Yellow St', '500000', '7', '6', '3500', '12345', 'Providence', 'RI', 'Townhouse'],
+    ['Orange St', '6000', '2', '4', '400', '12345', 'Newport', 'RI', 'Apartment'],
+    ['Purple St', '250000', '3', '2', '1800', '12346', 'Cranston', 'RI', 'House'],
+    ['Silver St', '180000', '2', '1', '1200', '12347', 'Pawtucket', 'RI', 'Apartment'],
+    ['Gold St', '350000', '4', '3', '2200', '12348', 'Johnston', 'RI', 'Townhouse'],
+    ['White St', '450000', '5', '4', '2800', '12349', 'Barrington', 'RI', 'House'],
+    ['Black St', '200000', '3', '2', '1600', '12350', 'Bristol', 'RI', 'House'],
+    ['Gray St', '320000', '4', '3', '2000', '12351', 'Woonsocket', 'RI', 'Townhouse']
+];
+
+const searchResultsGold = [
+    ['Gold St', '350000', '4', '3', '2200', '12348', 'Johnston', 'RI', 'Townhouse']
+];
+
